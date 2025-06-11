b1ad1477b574acac4e6b69626671f455b05780fd
Mateus Silva <mateus42silva@gmail.com>
2025-06-11 11:07:02 -0300

    modularize sales service

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -1,40 +1,46 @@
 const path = require('path');
 const { readCSV } = require('../utils/csvReader');
 const { normalizeCSV } = require('../utils/normalizeData');
-const { Transaction} = require('../models/Transaction');
+const { Transaction } = require('../models/Transaction');
 const { TransactionPair } = require('../models/TransactionPair');
 
 async function searchReturnedProducts() {
-  console.log("INICIANDO BUSCA")
+  const transactions = await loadTransactions();
+  const { salesOnly, refundsMap } = separateTransactions(transactions);
+  return pairTransactions(salesOnly, refundsMap);
+}
+
+async function loadTransactions() {
   const data = await readCSV(path.join(__dirname, '../data/sales_and_reversals.csv'));
   const normalized = normalizeCSV(data);
-  const transactions = normalized.map(sale => new Transaction(sale));
+  return normalized.map(sale => new Transaction(sale));
+}
 
+function separateTransactions(transactions) {
+  const salesOnly = [];
   const refundsMap = new Map();
 
-  const salesOnly = transactions.filter(t => !t.isReversal);
-  console.log("vendas ", salesOnly.length)
-  const refundsOnly = transactions.filter(t => t.isReversal);
-  console.log("reembolsos ", refundsOnly.length)
-
-  for (const refund of refundsOnly) {
-    const key = `${refund.product}-${refund.invoice}`;
-    if (!refundsMap.has(key)) {
-      refundsMap.set(key, []);
+  for (const t of transactions) {
+    if (t.isReversal) {
+      const key = `${t.product}-${t.invoice}`;
+      if (!refundsMap.has(key)) refundsMap.set(key, []);
+      refundsMap.get(key).push(t);
+    } else {
+      salesOnly.push(t);
     }
-    refundsMap.get(key).push(refund);
   }
 
+  return { salesOnly, refundsMap };
+}
+
+function pairTransactions(sales, refundsMap) {
   const result = [];
 
-  for (const sale of salesOnly) {
+  for (const sale of sales) {
     const key = `${sale.product}-${sale.invoice}`;
     const refunds = refundsMap.get(key);
-
     if (refunds && refunds.length > 0) {
-      console.log("Encontrado reembolso para venda", sale.invoice, sale.product);
       const refund = refunds.shift();
-      console.log("Novo par encontrado", sale, refund)
       result.push(new TransactionPair(sale, refund).toJSON());
     }
   }
